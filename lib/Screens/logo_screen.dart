41a5d777a6190a315cc0971c3b6e04c2072350b1
mahneenmirza <233555@students.au.edu.pk>
2025-05-01 20:48:33 +0500

    check

--- lib/Screens/logo_screen.dart
+++ lib/Screens/logo_screen.dart
@@ -3,32 +3,33 @@ import 'package:tour_guide_application/Screens/onboarding_screen.dart'; // Impor
 import 'package:tour_guide_application/Authentication/auth_gate.dart';
 
 class LogoScreen extends StatelessWidget {
+  const LogoScreen({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            // Centered Logo
-            Image.asset(
-              'assets/images/logo.png',
-              width: 150, // Adjust size as needed
-              height: 150,
-            ),
-            const SizedBox(height: 20),
+      body: Container(
+        color: Colors.teal, // Set teal background color
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // Centered Logo
+              Image.asset('assets/images/logo.png', width: 150, height: 150),
+              const SizedBox(height: 20),
 
-            // Welcome Line
-            Text(
-              'Your World, Our Guide',
-              style: const TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-                color: Colors.black,
+              // Welcome Line
+              const Text(
+                'Your World, Our Guide',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white, // Adjust text color for contrast
+                ),
+                textAlign: TextAlign.center,
               ),
-              textAlign: TextAlign.center,
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
