822573c7cffc079a6d4f2fd52f97d2884ecb66ff
Abdullah-577 <233577@students.au.edu.pk>
2025-05-12 03:14:01 +0500

    refactor: Update journey cards with new locations and pricing details

--- lib/Services/supabase_service.dart
+++ lib/Services/supabase_service.dart
@@ -1,54 +1,13 @@
-// // lib/services/supabase_service.dart
-// // ignore_for_file: unnecessary_null_comparison
-
-// import 'package:supabase_flutter/supabase_flutter.dart';
-
-// class SupabaseService {
-//   static final SupabaseService _instance = SupabaseService._internal();
-
-//   factory SupabaseService() {
-//     return _instance;
-//   }
-
-//   SupabaseService._internal();
-
-//   // Ensure the Supabase client is initialized and accessible
-//   Future<void> initializeSupabase() async {
-//     try {
-//       if (Supabase.instance.client == null) {
-//         // Initialize Supabase only if it's not initialized already
-//         await Supabase.initialize(
-//           url:
-//               'https://wkwhjswjekqlugndxegl.supabase.co', // Replace with your URL
-//           anonKey:
-//               'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indrd2hqc3dqZWtxbHVnbmR4ZWdsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI0Nzg3MTQsImV4cCI6MjA1ODA1NDcxNH0.VNb3DAheO5YBx0rtSrk0S9vh13MI3TQlN0VnICQRAJk', // Replace with your anonKey
-//         );
-//       }
-//     } catch (e) {
-//       print('Error initializing Supabase: $e');
-//       throw Exception('Failed to initialize Supabase');
-//     }
-//   }
-
-//   // Get the Supabase client
-//   SupabaseClient get supabase {
-//     if (Supabase.instance.client == null) {
-//       throw Exception('Supabase client is not initialized');
-//     }
-//     return Supabase.instance.client;
-//   }
-// }
-
-import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:supabase_flutter/supabase_flutter.dart' as supabase;
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import '../models/place_details.dart';
+
 
 class SupabaseService {
   /// Store user's current location in the 'locations' table
   static Future<void> storeLocation(String text, LatLng latLng) async {
-    final userId = Supabase.instance.client.auth.currentUser?.id;
+    final userId = supabase.Supabase.instance.client.auth.currentUser?.id;
     if (userId != null) {
-      await Supabase.instance.client.from('locations').insert({
+      await supabase.Supabase.instance.client.from('locations').insert({
         'user_id': userId,
         'location_text': text,
         'lat': latLng.latitude,
@@ -56,22 +15,5 @@ class SupabaseService {
       });
     }
   }
-
-  /// Fetch place details from the 'places' table by QR code ID
-  static Future<PlaceDetails?> fetchPlaceById(String id) async {
-    try {
-      final response = await Supabase.instance.client
-          .from('places')
-          .select()
-          .eq('id', id)
-          .single();
-
-      return PlaceDetails.fromMap(response);
-    } catch (e) {
-      print('Error fetching place: $e');
-      return null;
-    }
-  }
 }
-
 
