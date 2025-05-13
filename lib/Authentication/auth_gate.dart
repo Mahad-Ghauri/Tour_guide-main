e285f629ff5240ab24bee55f46f35fcd957df051
Mahad-Ghauri <mahadghauri222@gmail.com>
2025-05-14 01:03:53 +0500

    feat: Add splash screen image and refactor AuthGate for improved authentication flow

--- lib/Authentication/auth_gate.dart
+++ lib/Authentication/auth_gate.dart
@@ -1,53 +1,34 @@
-// // import 'package:flutter/material.dart';
-// // import 'package:supabase_flutter/supabase_flutter.dart';
-// // import 'package:tour_guide_application/Screens/home_screen.dart';
-// // import 'package:tour_guide_application/Screens/Authentication%20Screens/login_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:tour_guide_application/Authentication/auth_controller.dart';
+import 'package:tour_guide_application/Screens/Authentication%20Screens/login_screen.dart';
+import 'package:tour_guide_application/Screens/home_screen.dart';
+import 'package:tour_guide_application/Screens/onboarding_screen.dart';
 
+class AuthGate extends StatelessWidget {
+  static const String id = 'AuthGate';
+  const AuthGate({super.key});
 
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: AuthenticationController().supabase.auth.onAuthStateChange,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Scaffold(
+            body: Center(child: CircularProgressIndicator()),
+          );
+        }
+        if (snapshot.hasError) {
+          return Scaffold(body: Center(child: Text(snapshot.error.toString())));
+        }
+        final session = snapshot.hasData ? snapshot.data!.session : null;
 
-// import 'package:flutter/material.dart';
-// import 'package:supabase_flutter/supabase_flutter.dart';
-// import 'package:tour_guide_application/Screens/home_screen.dart';
-// import 'package:tour_guide_application/Screens/Authentication Screens/login_screen.dart';
-// import 'package:tour_guide_application/Screens/Authentication Screens/reset_password_screen.dart';
-
-// class AuthGate extends StatefulWidget {
-//   static const String id = 'AuthGate';
-
-//   const AuthGate({super.key});
-
-//   @override
-//   State<AuthGate> createState() => _AuthGateState();
-// }
-
-// class _AuthGateState extends State<AuthGate> {
-//   late final Stream<AuthState> _authStream;
-
-//   @override
-//   void initState() {
-//     super.initState();
-
-//     /// 🔁 Listen to auth state changes
-//     Supabase.instance.client.auth.onAuthStateChange.listen((data) async {
-//       final event = data.event;
-
-//       if (event == AuthChangeEvent.passwordRecovery) {
-//         // 🔐 Navigate to password reset screen
-//         Navigator.of(context).push(MaterialPageRoute(
-//           // builder: (_) => const ResetPasswordScreen(),
-//         ));
-//       }
-//     });
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     final session = Supabase.instance.client.auth.currentSession;
-
-//     if (session != null) {
-//       return HomeScreen(); // ✅ User is logged in
-//     } else {
-//       return LoginScreen(); // ❌ User is not logged in
-//     }
-//   }
-// }
+        if (session != null) {
+          return const HomeScreen();
+        } else {
+          return const OnboardingScreen(nextScreen: LoginScreen());
+        }
+      },
+    );
+  }
+}
