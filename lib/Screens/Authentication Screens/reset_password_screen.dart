a230392bf5126b270afe39190287cced846f0444
Mahad-Ghauri <mahadghauri222@gmail.com>
2025-05-28 23:26:46 +0500

    feat: Center title in AppBar and add back navigation in ResetPasswordScreen

--- lib/Screens/Authentication Screens/reset_password_screen.dart
+++ lib/Screens/Authentication Screens/reset_password_screen.dart
@@ -61,7 +61,11 @@ class _ResetPasswordScreenState extends State<ResetPasswordScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Reset Your Password'),
+        title: const Text('Reset Your Password' , centerTitle: true),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () => Navigator.of(context).pop(),
+        ),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16),
@@ -72,7 +76,7 @@ class _ResetPasswordScreenState extends State<ResetPasswordScreen> {
               'Enter your new password below and tap "Update Password".',
               style: TextStyle(fontSize: 16),
             ),
-            
+
             const SizedBox(height: 16),
             TextField(
               controller: _passwordController,
